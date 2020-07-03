e11a39df557529873497f245c527591be44f3b13
chengzi <ttghost@126.com>
2020-07-03 16:01:32 +0800

    feat: 添加inputNumber校验

--- src/widgets/inputNumber/View.vue
+++ src/widgets/inputNumber/View.vue
@@ -22,16 +22,15 @@
   //- edit mode
   van-field(
     v-else
-    type='text'
+    type='number'
     v-model='model[schema.key]'
     :name='schema.name'
     :placeholder='schema.placeholder'
     :disabled='schema.disabled'
     :size='rootSchema.size'
-    :rules='numberRules'
+    :rules='numberRules()'
     :label='schema.label'
     :required='required'
-    :formatter='formatter'
     :readonly='isDisplay'
     @change="event('on-change', ...arguments)"
     @focus="event('on-focus', ...arguments)"
@@ -41,36 +40,27 @@
 </template>
 <script>
 import viewExtend from '../../extends/view'
-import { formatNumber } from '../../util/format'
 
 export default {
   extends: viewExtend,
-  computed: {
+  methods: {
     numberRules () {
-      const { key, option } = this.schema
+      const { option, rules } = this.schema
       const { min, max } = option
-      const rule = { ...this.rules[key][0] }
-      const result = [rule]
-      rule.validator = function (value, rule) {
-        return (typeof value === 'number') || !!value
-      }
-      const rule2 = {
-        trigger: rule.trigger,
+      const rule = { ...rules[0] }
+      return [{
+        message: rule.message,
+        validator: function (value, rule) {
+          return (typeof value === 'number') || !!value
+        }
+      }, {
+        message: `请输入不小于 ${min} 且 不大于 ${max}的数字`,
         validator: function (value, rule) {
           const number = parseFloat(value)
           const finalValue = isNaN(number) ? 0 : number
           return (finalValue >= min) && (finalValue <= max)
-        },
-        message: `请输入不小于 ${min} 且 不大于 ${max}的数字`
-      }
-
-      result.push(rule2)
-      return result
-    }
-  },
-  methods: {
-    formatter (value) {
-      return formatNumber(value, true)
+        }
+      }]
     }
   }
 }
